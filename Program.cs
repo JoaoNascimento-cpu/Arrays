53da8e53ff705d786a145431f8c5e98eabf97873
João Nascimento <jhonyzito4gb@gmail.com>
2022-06-17 14:52:15 -0300

    arrays feito

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+
+            var funcionarios = new Funcionarios[10];
+            
+
+            foreach (var funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario.Id);
+
+            }
+        }
+
+        public struct Funcionarios
+        {
+            public int Id { get; set; }
         }
     }
 }
